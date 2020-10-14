567b60317b66e154a639c0fe71e98a86febc5fb9
account0123 <ianparra200@gmail.com>
2020-10-14 15:45:40 -0300

    semicolon added

--- examples/WordOfTheDay.js
+++ examples/WordOfTheDay.js
@@ -6,7 +6,7 @@ const rae = new RAE(debug);
 async function start(){
 	const wotd = await rae.getWordOfTheDay();
 	
-	const word = wotd.getHeader()
+	const word = wotd.getHeader();
 	console.log(`La palabra del día es: ${word}\n`);
 }
 
