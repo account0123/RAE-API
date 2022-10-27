d9bfbe11fafeb9c062b5417a4691b395074f3d8c
DanLop <89615008+DanLop618@users.noreply.github.com>
2022-10-27 17:59:28 -0500

    Implementación de método
    Se implementó un método para obtener una palabra aleatoria de la RAE, incluyendo su ID y definiciones (con tipo).

---
+++ HttpInterface.js
@@ -0,0 +1,98 @@
+const { BASE_URL, AUTH } = require('./Constants');
+const https = require('https');
+const {IncomingMessage} = require('http');
+const Utils = require("./Utils");
+const RAE = require('./RAE');
+
+const httpsAgent = new https.Agent({ ALPNProtocols: ['http/1.1'], minVersion: 'TLSv1.3', maxVersion: 'TLSv1.3' });
+
+class HttpInterface {
+	debug;
+	truncatedDebug;
+	setDebug(bool){
+		this.debug = bool;
+	}
+	setTruncatedDebug(bool){
+		this.truncatedDebug = bool;
+	}
+	/**
+	 * Sends a GET request to URL/endpoint
+	 * @param {string} endpoint
+	 * @returns {Promise<IncomingMessage>} Http Response
+	 */
+	sendRequest(endpoint){
+		const url = BASE_URL + endpoint;
+		const headers =
+			{
+				'User-Agent':'Diccionario/2 CFNetwork/808.2.16 Darwin/16.3.0',
+				'Content-Type':'application/x-www-form-urlencoded',
+				'Authorization': AUTH
+			};
+		return new Promise((resolve, reject)=>{
+			https.get(url, {headers: headers, agent: httpsAgent},  (res) => {
+    		let body = '';
+
+			if(res.statusCode != 200) reject('Llamada no disponible en este momento.');
+    		// called when a data chunk is received.
+    		res.on('data', (chunk) => {
+				let data = chunk.toString().replace(/\n/g,'');
+				data = data.replace(/\t/g, '');
+				if(data.indexOf('json(') != -1){
+					data = data.slice(5, -1);
+				}if (data.indexOf('jsonp123(') != -1){
+					data = data.slice(9,-1)
+				}
+				if(Array.isArray(data)) data = JSON.stringify(data);
+				body += data;
+    		});
+
+    		// called when the complete response is received.
+    		res.on('end', () => {
+				// deletes <sup>1<\/sup>, which was altering a near condition
+				body = body.replace(/<sup>\d*<\\\/sup>/g, '');
+				// fetch word indexed by "Véase" (see also)
+				if(body.match(/^<article id=\".*\">/)){
+					const i = body.match(/id="(\w+)"/)[1];
+					const h = body.match(/<header [^>]*>([^<]+)/)[1]
+						.replace(/&#xE1;/g, 'á')
+						.replace(/&#xE9;/g, 'é')
+						.replace(/&#xED;/g, 'í')
+						.replace(/&#xF3;/g, 'ó')
+						.replace(/&#xFA;/g, 'ú')
+						.replace(/&#xF1;/g, 'ñ');
+					body = JSON.parse(Utils.get_definitions(body));
+					body["id"] = i;
+					body["header"] = h;
+					body = JSON.stringify(body);
+				}else if(body.match(/^<abbr title="V&#xE9;ase"/)){
+					const i = body.match(/id="(\w+)"/)[1];
+					this.sendRequest('fetch?id=' + i).then((res)=>resolve(res));
+					return;
+				}else if(body != body.replace(/<\/?[^>]+(>|$)/g, '') && !body.startsWith('{')){
+					body = Utils.get_definitions(body);
+				}
+				if(this.debug){
+					console.log('fetch: GET ' + BASE_URL + endpoint);
+					console.log('headers: ' + JSON.stringify(res.headers));
+					console.log('body: ' + body);
+					console.log('Status Code: ' + res.statusCode);
+				}
+				if(!this.debug && this.truncatedDebug){
+					console.log('fetch: GET ' + BASE_URL + endpoint);
+					console.log('body: ' + body);
+					console.log('Status Code: ' + res.statusCode);
+				}
+				const response = res;
+				Object.defineProperty(response, 'body', { value: JSON.parse(body), writable: false});
+				resolve(response);
+			});
+
+			}).on("error", (err) => {
+				console.log("Error: ", err.message);
+				reject(err);
+			});
+		});
+	}
+}
+
+module.exports = HttpInterface;
---
+++ RandomWordResponse.js
@@ -0,0 +1,59 @@
+const Response = require('../Response');
+const {IncomingMessage} = require('http');
+
+/**
+ * WordOfTheDayResponse.
+ *
+ * @method string getHeader()
+ * @method string getId()
+ * @method bool isHeader()
+ * @method bool isId()
+ */
+class RandomWordResponse extends Response{
+	#definitions;
+	#header;
+	#id;
+	/**
+	 *
+	 * @param {IncomingMessage} response
+	 */
+	constructor(response){
+		super(response);
+		const body = response.body;
+		if (body.header) this.#header = body.header;
+		if (body.id) this.#id = body.id;
+		if (body.definitions) this.#definitions = body.definitions;
+	}
+
+	/**
+	 * @returns {boolean} Sí o no hay la palabra tiene encabezado.
+	 */
+	isHeader(){ return this.#header ? true : false;}
+
+	/**
+	 * @returns {boolean} Sí o no la palabra tiene id.
+	 */
+	isId(){ return this.#id ? true : false; }
+
+	/**
+	 * @returns {boolean} Sí o no la palabra tiene definición.
+	 */
+	isDefinition(){ return this.#definitions ? true : false; }
+
+	/**
+	 * @returns {string} El encabezado de la palabra.
+	 */
+	getHeader(){ return this.#header; }
+
+	/**
+	 * @returns {string} La id de la palabra.
+	 */
+	getId(){ return this.#id; }
+
+	/**
+	 * @returns {object[]} Las definiciones de la palabra en formato JSON.
+	 */
+	getDefinitions(){ return this.#definitions; }
+}
+
+module.exports = RandomWordResponse;
